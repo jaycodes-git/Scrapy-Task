8d54a8db8384974f4a8b07240fe37cd900b0e10a
Kishan Sharma <80383998+jaycodes-git@users.noreply.github.com>
2021-03-10 15:03:45 +0530

    Update items.py

--- task1/task1/items.py
+++ task1/task1/items.py
@@ -1,14 +1,10 @@
 # Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
 
 
 class Task1Item(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+    
     name = scrapy.Field()
     price = scrapy.Field()
     manf = scrapy.Field()
